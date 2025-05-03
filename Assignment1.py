ba353bf7b4631d8dc919268a2d0889c5b02240fd
gramshied <116554480+gramshied@users.noreply.github.com>
2025-05-03 18:19:22 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,13 @@
+first=input('Enter the first Number:')
+first=float(first)
+second=input('Enter the second Number:')
+second=float(second)
+#Addition
+print('Addition', first+second)
+
+#substraction
+print('Substraction',first-second)
+#multiplication
+print('Multiplication', first*second)
+#Division
+print('Division',first/second)
---
+++ Assignment1_task2.py
@@ -0,0 +1,5 @@
+firstname=input('Enter your first Name:')
+surename=input('Enter your last Name:')
+
+
+print(f"\nHello, {firstname} {surename}! Welcome to the Python program.")
